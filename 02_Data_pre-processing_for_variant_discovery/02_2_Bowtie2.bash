3180cf9b8c09203a52eb87d56d44cc16a817ee7e
Jaewoong Lee <230@fumire.moe>
2023-04-30 21:48:40 +0900

    Update recipes

--- 02_Data_pre-processing_for_variant_discovery/02_2_Bowtie2.bash
+++ 02_Data_pre-processing_for_variant_discovery/02_2_Bowtie2.bash
@@ -2,5 +2,5 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-python3 02_2_Bowtie2.py /BiO/Store/Standard-Pipeline/cn95N_S0_L009_R1_001.fastq.gz /BiO/Store/Standard-Pipeline/cn95N_S0_L009_R2_001.fastq.gz cn95N.Bowtie2.bam
-python3 02_2_Bowtie2.py /BiO/Store/Standard-Pipeline/cn95P_S0_L009_R1_001.fastq.gz /BiO/Store/Standard-Pipeline/cn95P_S0_L009_R2_001.fastq.gz cn95P.Bowtie2.bam
+python3 02_2_Bowtie2.py /BiO/Store/Standard-Pipeline/cn95N_S0_L009_R1_001.fastq.gz /BiO/Store/Standard-Pipeline/cn95N_S0_L009_R2_001.fastq.gz .
+python3 02_2_Bowtie2.py /BiO/Store/Standard-Pipeline/cn95P_S0_L009_R1_001.fastq.gz /BiO/Store/Standard-Pipeline/cn95P_S0_L009_R2_001.fastq.gz .
