31e7bda931b80062f96405053316d14eea432655
Jaewoong Lee <230@fumire.moe>
2023-05-03 10:47:29 +0900

    Add VCF >> MAF pipeline

--- 03_Somatic_short_variant_discovery/03_1_Mutect2.py
+++ 03_Somatic_short_variant_discovery/03_1_Mutect2.py
@@ -14,7 +14,7 @@ parser.add_argument("tumor", help="Tumor BAM file")
 parser.add_argument("output", help="Output MAF file")
 parser.add_argument("-c", "--config", help="config INI file", default="../config.ini")
 
-parser.add_argument("-p", "--PON", help="Panel of normal (optional)", default=True, action="store_true")
+parser.add_argument("-p", "--PON", help="Panel of normal (optional)", default=False, action="store_true")
 
 args = parser.parse_args()
 
@@ -40,7 +40,7 @@ if args.PON:
 
     with open(f"Filter_{normal_name}.sh", "w") as sh:
         sh.write("#!/bin/bash\n")
-        sh.write("{config['TOOLS']['gatk']} FilterMutectCalls --reference {config['REFERENCES']['fasta']} --variant {output_directory}/{normal_name}.vcf --output {output_directory}/{normal_name}.filter.vcf")
+        sh.write(f"{config['TOOLS']['gatk']} FilterMutectCalls --reference {config['REFERENCES']['fasta']} --variant {output_directory}/{normal_name}.vcf --output {output_directory}/{normal_name}.filter.vcf")
 
     filter_normal_job_id = subprocess.check_output(f"sbatch --dependency=afterok:{mutect_normal_job_id} --chdir=$(realpath .) --cpus-per-task=1 --error='%x-%A.txt' --job-name='Filter_{normal_name}' --mem={int(config['DEFAULT']['memory']) // int(config['DEFAULT']['threads'])}G --output='%x-%A.txt' --export=ALL Filter_{normal_name}.sh", encoding="utf-8", shell=True).split()[-1]
 
@@ -49,3 +49,58 @@ if args.PON:
         sh.write(f"{config['TOOLS']['awk']} -F '\t' '{{if($0 ~ /\\#/) print; else if($7 == \"PASS\") print}}' {output_directory}/{normal_name}.filter.vcf > {output_directory}/{normal_name}.PASS.vcf")
 
     PASS_normal_job_id = subprocess.check_output(f"sbatch --dependency=afterok:{filter_normal_job_id} --chdir=$(realpath .) --cpus-per-task=1 --error='%x-%A.txt' --job-name='PASS_{normal_name}' --mem={int(config['DEFAULT']['memory']) // int(config['DEFAULT']['threads'])}G --output='%x-%A.txt' --export=ALL PASS_{normal_name}.sh", encoding="utf-8", shell=True).split()[-1]
+
+    with open(f"Index_{normal_name}.sh", "w") as sh:
+        sh.write("#!/bin/bash\n")
+        sh.write(f"{config['TOOLS']['gatk']} IndexFeatureFile --input {output_directory}/{normal_name}.PASS.vcf --output {output_directory}/{normal_name}.PASS.vcf.idx")
+
+    Index_normal_job_id = subprocess.check_output(f"sbatch --dependency=afterok:{PASS_normal_job_id} --chdir=$(realpath .) --cpus-per-task=1 --error='%x-%A.txt' --job-name='Index_{normal_name}' --mem={int(config['DEFAULT']['memory']) // int(config['DEFAULT']['threads'])}G --output='%x-%A.txt' --export=ALL Index_{normal_name}.sh", encoding="utf-8", shell=True).split()[-1]
+
+    with open("DB.sh", "w") as sh:
+        sh.write("#!/bin/bash\n")
+        sh.write(f"{config['TOOLS']['gatk']} GenomicsDBImport --reference {config['REFERENCES']['fasta']} --genomicsdb-workspace-path {output_directory}/DB --variant {output_directory}/{normal_name}.PASS.vcf --reader-threads {config['DEFAULT']['threads']} --max-num-intervals-to-import-in-parallel {config['DEFAULT']['threads']} --overwrite-existing-genomicsdb-workspace true")
+
+    DB_job_id = subprocess.check_output(f"sbatch --dependency=afterok:{Index_normal_job_id} --chdir=$(realpath .) --cpus-per-task={config['DEFAULT']['threads']} --error='%x-%A.txt' --job-name='DB_{normal_name}' --mem={config['DEFAULT']['memory']}G --output='%x-%A.txt' --export=ALL DB_{normal_name}.sh", encoding="utf-8", shell=True).split()[-1]
+
+    with open("PON.sh", "w") as sh:
+        sh.write("#!/bin/bash\n")
+        sh.write(f"{config['TOOLS']['gatk']} CreateSomaticPanelOfNormals --reference {config['REFERENCES']['fasta']} --variant 'gendb://{output_directory}/DB' --output {output_directory}/PON.vcf.gz")
+
+    PON_job_id = subprocess.check_output(f"sbatch --dependency=afterok:{DB_job_id} --chdir=$(realpath .) --cpus-per-task=1 --error='%x-%A.txt' --job-name='PON_{normal_name}' --mem={int(config['DEFAULT']['memory']) // int(config['DEFAULT']['threads'])}G --output='%x-%A.txt' --export=ALL PON_{normal_name}.sh", encoding="utf-8", shell=True).split()[-1]
+
+if args.PON:
+    with open(f"Mutect_{tumor_name}.sh", "w") as sh:
+        sh.write("#!/bin/bash\n")
+        sh.write(f"{config['TOOLS']['gatk']} Mutect2 --reference {config['REFERENCES']['fasta']} --input {args.normal} --input {args.tumor} --normal-sample {normal_name} --output {output_directory}/{tumor_name}.vcf --panel-of-normals {output_directory}/PON.vcf.gz --native-pair-hmm-threads {config['DEFAULT']['threads']}")
+
+    mutect_tumor_job_id = subprocess.check_output(f"sbatch --dependency=afterok:{PON_job_id} --chdir=$(realpath .) --cpus-per-task={config['DEFAULT']['threads']} --error='%x-%A.txt' --job-name='Mutect_{tumor_name}' --mem={config['DEFAULT']['memory']}G --output='%x-%A.txt' --export=ALL Mutect_{tumor_name}.sh", encoding="utf-8", shell=True).split()[-1]
+else:
+    with open(f"Mutect_{tumor_name}.sh", "w") as sh:
+        sh.write("#!/bin/bash\n")
+        sh.write(f"{config['TOOLS']['gatk']} Mutect2 --reference {config['REFERENCES']['fasta']} --input {args.normal} --input {args.tumor} --normal-sample {normal_name} --output {output_directory}/{tumor_name}.vcf --native-pair-hmm-threads {config['DEFAULT']['threads']}")
+
+    mutect_tumor_job_id = subprocess.check_output(f"sbatch --chdir=$(realpath .) --cpus-per-task={config['DEFAULT']['threads']} --error='%x-%A.txt' --job-name='Mutect_{tumor_name}' --mem={config['DEFAULT']['memory']}G --output='%x-%A.txt' --export=ALL Mutect_{tumor_name}.sh", encoding="utf-8", shell=True).split()[-1]
+
+with open(f"Filter_{tumor_name}.sh", "w") as sh:
+    sh.write("#!/bin/bash\n")
+    sh.write(f"{config['TOOLS']['gatk']} FilterMutectCalls --reference {config['REFERENCES']['fasta']} --variant {output_directory}/{tumor_name}.vcf --output {output_directory}/{tumor_name}.filter.vcf")
+
+filter_tumor_job_id = subprocess.check_output(f"sbatch --dependency=afterok:{mutect_tumor_job_id} --chdir=$(realpath .) --cpus-per-task=1 --error='%x-%A.txt' --job-name='Filter_{tumor_name}' --mem={int(config['DEFAULT']['memory']) // int(config['DEFAULT']['threads'])}G --output='%x-%A.txt' --export=ALL Filter_{tumor_name}.sh", encoding="utf-8", shell=True).split()[-1]
+
+with open(f"PASS_{tumor_name}.sh", "w") as sh:
+    sh.write("#!/bin/bash\n")
+    sh.write(f"{config['TOOLS']['awk']} -F '\t' '{{if($0 ~ /\\#/) print; else if($7 == \"PASS\") print}}' {output_directory}/{tumor_name}.filter.vcf > {output_directory}/{tumor_name}.PASS.vcf")
+
+PASS_tumor_job_id = subprocess.check_output(f"sbatch --dependency=afterok:{filter_tumor_job_id} --chdir=$(realpath .) --cpus-per-task=1 --error='%x-%A.txt' --job-name='PASS_{tumor_name}' --mem={int(config['DEFAULT']['memory']) // int(config['DEFAULT']['threads'])}G --output='%x-%A.txt' --export=ALL PASS_{tumor_name}.sh", encoding="utf-8", shell=True).split()[-1]
+
+with open(f"Index_{tumor_name}.sh", "w") as sh:
+    sh.write("#!/bin/bash\n")
+    sh.write(f"{config['TOOLS']['gatk']} IndexFeatureFile --input {output_directory}/{tumor_name}.PASS.vcf --output {output_directory}/{tumor_name}.PASS.vcf.idx")
+
+Index_tumor_job_id = subprocess.check_output(f"sbatch --dependency=afterok:{PASS_tumor_job_id} --chdir=$(realpath .) --cpus-per-task=1 --error='%x-%A.txt' --job-name='Index_{tumor_name}' --mem={int(config['DEFAULT']['memory']) // int(config['DEFAULT']['threads'])}G --output='%x-%A.txt' --export=ALL Index_{tumor_name}.sh", encoding="utf-8", shell=True).split()[-1]
+
+with open(f"MAF_{tumor_name}.sh", "w") as sh:
+    sh.write("#!/bin/bash\n")
+    sh.write(f"{config['TOOLS']['vcf2maf']} --vep-path {config['TOOLS']['vep']} --vep-data {config['TOOLS']['vep']} --vep-forks {config['DEFAULT']['threads']} --ncbi-build 'GRCh38' --input-vcf {output_directory}/{tumor_name}.PASS.vcf --output {output_directory}/{tumor_name}.PASS.maf --tumor-id {tumor_name} --normal-id {normal_name} --ref-fasta {config['REFERENCES']['fasta']}")
+
+MAF_job_id = subprocess.check_output(f"sbatch --dependency=afterok:{Index_tumor_job_id} --chdir=$(realpath .) --cpus-per-task={config['DEFAULT']['threads']} --error='%x-%A.txt' --job-name='MAF_{tumor_name}' --mem={config['DEFAULT']['memory']}G --output='%x-%A.txt' --export=ALL MAF_{tumor_name}.sh", encoding="utf-8", shell=True).split()[-1]
