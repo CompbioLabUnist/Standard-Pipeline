eaa5d64e9bd4f8a9da3fd79b6af296d147f2d946
Jaewoong Lee <230@fumire.moe>
2025-02-03 16:37:06 +0900

    Update 01_QC
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- 01_Quality_check/01_1_FastQC.bash
+++ 01_Quality_check/01_1_FastQC.bash
@@ -2,4 +2,4 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-python3 01_1_FastQC.py /BiO/Store/Standard-Pipeline/cn95N_S0_L009_R1_001.fastq.gz /BiO/Store/Standard-Pipeline/cn95N_S0_L009_R2_001.fastq.gz /BiO/Store/Standard-Pipeline/cn95P_S0_L009_R1_001.fastq.gz /BiO/Store/Standard-Pipeline/cn95P_S0_L009_R2_001.fastq.gz
+python3 01_1_FastQC.py $(realpath ../00_Data/WES/C001_TN_DNA_R1.fastq.gz)
--- 01_Quality_check/FastQC_0.sh
+++ 01_Quality_check/FastQC_0.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/BiO/Share/Tools/FastQC/fastqc --outdir /Users/fumire/Standard-Pipeline/01_Quality_check --format fastq --noextract --threads 20 /BiO/Store/Standard-Pipeline/cn95N_S0_L009_R1_001.fastq.gz
+/BiO/Share/Tools/FastQC_v0.12.1/fastqc --outdir /BiO/Teach/Standard-Pipeline/01_Quality_check --format fastq --noextract --threads 10 /BiO/Store/UNIST-ApocrineCarcinoma-SMC-2021-04/WES/C001_TN_DNA_R1.fastq.gz
