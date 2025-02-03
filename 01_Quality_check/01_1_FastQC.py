45805f909b3a36d595e60693be55b1c6e7e5d14c
Jaewoong Lee <230@fumire.moe>
2025-02-03 16:51:09 +0900

    Update pipeline
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- 01_Quality_check/01_1_FastQC.py
+++ 01_Quality_check/01_1_FastQC.py
@@ -3,26 +3,45 @@
 01_1_FastQC.py: Quality check with FastQC
 """
 import argparse
-import configparser
 import os
+import sys
+from pipeline_utils import PipelineManagerBase
 
-parser = argparse.ArgumentParser()
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-parser.add_argument("input", help="FASTQ file", nargs="+")
-parser.add_argument("-o", "--output", help="Output directory", default=os.getcwd())
-parser.add_argument("-c", "--config", help="config INI file", default="../config.ini")
 
-parser.add_argument("-n", "--dryrun", help="Don't actually run any recipe; just make .SH only", default=False, action="store_true")
+class PipelineManager(PipelineManagerBase):
+    def __init__(self, input_files, output, config_file, dryrun):
+        super().__init__(config_file, dryrun, output_dir=output)
+        self.input_files = sorted(input_files)
 
-args = parser.parse_args()
+        self.name = self.input_files[0][self.input_files[0].rfind("/") + 1:self.input_files[0].find("_DNA")]
 
-config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
-config.read(args.config)
+    def run_fastqc(self):
+        command = f"{self.config['TOOLS']['fastqc']} --outdir {self.output_dir} --format fastq --noextract --threads {self.config['DEFAULT']['threads']} {self.input_files[0]}"
+        self.create_sh("FastQC", command)
+        return self.submit_job("FastQC")
 
-for i, f in enumerate(args.input):
-    with open(f"FastQC_{i}.sh", "w") as sh:
-        sh.write("#!/bin/bash\n")
-        sh.write(f"{config['TOOLS']['fastqc']} --outdir {args.output} --format fastq --noextract --threads {config['DEFAULT']['threads']} {f}")
 
-    if not args.dryrun:
-        os.system(f"sbatch --chdir=$(realpath .) --cpus-per-task={config['DEFAULT']['threads']} --error='%x-%A.txt' --job-name='FastQC_{i}' --mem={config['DEFAULT']['memory']}G --output='%x-%A.txt' --export=ALL FastQC_{i}.sh")
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input", help="FASTQ file", nargs="+")
+    parser.add_argument("-o", "--output", help="Output directory", default=os.getcwd())
+    parser.add_argument("-c", "--config", help="config INI file", default="../config.ini")
+    parser.add_argument("-n", "--dryrun", help="Don't actually run any recipe; just make .SH only", default=False, action="store_true")
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+
+    pipeline = PipelineManager(input_files=args.input, output=args.output, config_file=args.config, dryrun=args.dryrun)
+
+    pipeline.create_dir()
+    pipeline.run_fastqc()
+
+
+if __name__ == "__main__":
+    main()
