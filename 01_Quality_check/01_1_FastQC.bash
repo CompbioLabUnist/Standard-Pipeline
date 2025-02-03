f785369fde013edfa594ea69e7fa7a22ce58ab2f
Jaewoong Lee <230@fumire.moe>
2025-02-03 16:51:58 +0900

    Update SH file
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- 01_Quality_check/01_1_FastQC.bash
+++ 01_Quality_check/01_1_FastQC.bash
@@ -2,4 +2,4 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-python3 01_1_FastQC.py $(realpath ../00_Data/WES/C001_TN_DNA_R1.fastq.gz)
+python3 -B 01_1_FastQC.py $(realpath ../00_Data/WES/C001_TN_DNA_R1.fastq.gz)
