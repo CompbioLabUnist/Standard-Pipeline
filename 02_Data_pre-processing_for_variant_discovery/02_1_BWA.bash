2e47bc95adcbb0c7bf54087e4f8a6443789b5ce4
Jaewoong Lee <230@fumire.moe>
2025-02-04 15:16:59 +0900

    typo
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- 02_Data_pre-processing_for_variant_discovery/02_1_BWA.bash
+++ 02_Data_pre-processing_for_variant_discovery/02_1_BWA.bash
@@ -3,4 +3,4 @@ set -euo pipefail
 IFS=$'\n\t'
 
 python3 -B 02_1_BWA.py $(realpath ../00_Data/WES/C001_TN_DNA_R1.fastq.gz ../00_Data/WES/C001_TN_DNA_R2.fastq.gz) $(realpath .)
-python3 -B 02_1_BWA.py $(realpath ../00_Data/WES/C001_TT_DNA_R1.fastq.gz ../00_Data/WES/C001_TT_DNA_R2.fastq.gz) $(realpath .)
+python3 -B 02_1_BWA.py $(realpath ../00_Data/WES/merge/C001_TT_DNA_R1.fastq.gz ../00_Data/WES/merge/C001_TT_DNA_R2.fastq.gz) $(realpath .)
