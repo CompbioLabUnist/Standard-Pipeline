56f25dd87fe0cdbba473f9d714afd644603eea95
Jaewoong Lee <230@fumire.moe>
2025-02-10 14:45:33 +0900

    typo
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- 03_Somatic_short_variant_discovery/03_1_Mutect2.bash
+++ 03_Somatic_short_variant_discovery/03_1_Mutect2.bash
@@ -2,4 +2,4 @@
 set -euo pipefail
 #IFS=$'\n\t'
 
-python3 -B 03_1_Mutect2.py $(realpath ../02_Data_pre-processing_for_variant_discovery/C001_TN.Sort.MarkDuplicates.BQSR.bam ../02_Data_pre-processing_for_variant_discovery/C001_TT.Sort.MarkDuplicates.BQSR.bam) $(realpath ./cn95.maf)
+python3 -B 03_1_Mutect2.py $(realpath ../02_Data_pre-processing_for_variant_discovery/C001_TN.Sort.MarkDuplicates.BQSR.bam ../02_Data_pre-processing_for_variant_discovery/C001_TT.Sort.MarkDuplicates.BQSR.bam) $(realpath ./C001.maf)
