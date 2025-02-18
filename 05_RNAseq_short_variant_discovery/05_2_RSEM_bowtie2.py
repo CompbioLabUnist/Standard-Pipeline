14a0ed25f541a376dc192cd40e5a4c0a0528ffa5
Jaewoong Lee <230@fumire.moe>
2025-02-18 18:54:54 +0900

    Add bowtie2 mapping
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

---
+++ 05_RNAseq_short_variant_discovery/05_2_RSEM_bowtie2.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+05_2_RSEM_bowtie2.py: RSEM - align reads with Bowtie2
+"""
+import argparse
+import os
+import sys
+from pipeline_utils import PipelineManagerBase
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+
+class PipelineManager(PipelineManagerBase):
+    def __init__(self, input_file_list, reference, output, config_file, dryrun):
+        super().__init__(config_file, dryrun)
+        self.input = list(map(os.path.realpath, sorted(input_file_list)))
+        self.reference = os.path.realpath(reference)
+        self.reference = self.reference[:self.reference.rfind(".")]
+        self.output = os.path.realpath(output)
+        self.name = self.output.split("/")[-1]
+
+    def run_bowtie2(self, dependency_id=None):
+        command = f"{self.config['TOOLS']['rsem_directory']}/rsem-calculate-expression --num-threads {self.config['DEFAULT']['threads']} --calc-pme --calc-ci --bowtie2 --bowtie2-path {os.path.dirname(self.config['TOOLS']['bowtie2'])} --output-genome-bam --sort-bam-by-coordinate --estimate-rspd --time --paired-end {self.input[0]} {self.input[1]} {self.reference} {self.output}"
+
+        self.create_sh("Bowtie2", command)
+        return self.submit_job("Bowtie2", dependency_id=dependency_id)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input", help="Input FASTQ files", nargs=2)
+    parser.add_argument("reference", help="Reference GRP file")
+    parser.add_argument("output", help="Output file basename")
+    parser.add_argument("-c", "--config", help="config INI file", default="../config.ini")
+    parser.add_argument("-n", "--dryrun", help="Don't actually run any recipe; just make .SH only", default=False, action="store_true")
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+
+    pipeline = PipelineManager(input_file_list=args.input, reference=args.reference, output=args.output, config_file=args.config, dryrun=args.dryrun)
+
+    pipeline.create_dir()
+
+    pipeline.run_bowtie2()
+
+
+if __name__ == "__main__":
+    main()
