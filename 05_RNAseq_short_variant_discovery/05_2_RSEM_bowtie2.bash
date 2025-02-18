b298a911236582630055e1ba5a5f33e80285458b
Jaewoong Lee <230@fumire.moe>
2025-02-18 20:36:38 +0900

    Update
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- 05_RNAseq_short_variant_discovery/05_2_RSEM_bowtie2.bash
+++ 05_RNAseq_short_variant_discovery/05_2_RSEM_bowtie2.bash
@@ -2,4 +2,4 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-python3 -B 05_2_RSEM_bowtie2.py $(realpath ../00_Data/WTS/C001_TT_RNA_R1.fastq.gz ../00_Data/WTS/C001_TT_RNA_R2.fastq.gz) $(realpath hg38.grp) $(realpath C001_TT)
+python3 -B 05_2_RSEM_bowtie2.py $(realpath ../00_Data/WTS/C001_TT_RNA_R1.fastq.gz ../00_Data/WTS/C001_TT_RNA_R2.fastq.gz) $(realpath hg38.grp) $(realpath C001_TT.bowtie2)
