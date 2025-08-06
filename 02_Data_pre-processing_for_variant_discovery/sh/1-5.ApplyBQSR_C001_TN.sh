2883cd59fe12aba5d8322e17088177ad9e100770
Jaewoong Lee <230@fumire.moe>
2025-08-06 20:39:28 +0900

    Add example SH files
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

---
+++ 02_Data_pre-processing_for_variant_discovery/sh/1-5.ApplyBQSR_C001_TN.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/BiO/Share/Tools/gatk-4.6.1.0/gatk ApplyBQSR --bqsr-recal-file /BiO/Teach/Standard-Pipeline/02_Data_pre-processing_for_variant_discovery/C001_TN.Sort.MarkDuplicates.BQSR.table --input /BiO/Teach/Standard-Pipeline/02_Data_pre-processing_for_variant_discovery/C001_TN.Sort.MarkDuplicates.bam --output /BiO/Teach/Standard-Pipeline/02_Data_pre-processing_for_variant_discovery/C001_TN.Sort.MarkDuplicates.BQSR.bam --reference /BiO/Share/Tools/gatk-bundle/hg38/Homo_sapiens_assembly38.fasta --create-output-bam-index true
