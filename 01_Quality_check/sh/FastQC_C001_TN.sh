9920d10324c26205491d01a78fc0c4ce3841db14
Jaewoong Lee <230@fumire.moe>
2026-03-13 15:02:26 +0900

    Update SH file
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- 01_Quality_check/sh/FastQC_C001_TN.sh
+++ 01_Quality_check/sh/FastQC_C001_TN.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-/BiO/Share/Tools/FastQC_v0.12.1/fastqc --outdir /BiO/Teach/Standard-Pipeline/01_Quality_check --format fastq --noextract --threads 10 /BiO/Store/UNIST-ApocrineCarcinoma-SMC-2021-04/WES/C001_TN_DNA_R1.fastq.gz
+#SBATCH --cpus-per-task=30
+#SBATCH --error='/BiO/Teach/Standard-Pipeline/01_Quality_check/stdeo/%x-%A.txt'
+#SBATCH --output='/BiO/Teach/Standard-Pipeline/01_Quality_check/stdeo/%x-%A.txt'
+#SBATCH --job-name='FastQC_C001_TN'
+#SBATCH --mem=55G
+#SBATCH --export=ALL
+/BiO/Share/Tools/FastQC_v0.12.1/fastqc --outdir /BiO/Teach/Standard-Pipeline/01_Quality_check --format fastq --noextract --threads 30 /BiO/Store/UNIST-ApocrineCarcinoma-SMC-2021-04/WES/C001_TN_DNA_R1.fastq.gz
