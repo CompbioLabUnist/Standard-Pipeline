c7c0305e09cbcf16208769d6d1d54114fe256195
Jaewoong Lee <230@fumire.moe>
2026-03-29 17:29:41 +0900

    Chromosome selection
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- 06_Somatic_variant_visualization/06_5_draw_rainfall.py
+++ 06_Somatic_variant_visualization/06_5_draw_rainfall.py
@@ -63,7 +63,7 @@ def main():
     input_data["Distance"] = distance_list
     print(input_data)
 
-    chromosome_list = "chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY chrM".split()
+    chromosome_list = "chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX ".split()
     print(len(chromosome_list), chromosome_list)
 
     coloring = {"C>A": "tab:blue", "C>G": "tab:orange", "C>T": "tab:green", "T>A": "tab:purple", "T>C": "tab:brown", "T>G": "tab:olive", "Indel": "tab:gray"}
