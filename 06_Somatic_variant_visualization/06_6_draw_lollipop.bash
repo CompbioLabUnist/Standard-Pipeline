c86d94462dc11817e97117d461408a918dfcaa16
Jaewoong Lee <230@fumire.moe>
2026-04-06 19:40:56 +0900

    Bug fixed: typo on code
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- 06_Somatic_variant_visualization/06_6_draw_lollipop.bash
+++ 06_Somatic_variant_visualization/06_6_draw_lollipop.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p Lollipop
-python3 06_6_draw_lollipop.py "$(realpath merged.tsv)" "$(realpath Lollipop)" --gene "TP53" "PIK3CA"
+python3 06_6_draw_lollipop.py "$(realpath merged.tsv)" "$(realpath Lollipop)" --gene "TP53" "GZMB"
--- 06_Somatic_variant_visualization/06_6_draw_lollipop.py
+++ 06_Somatic_variant_visualization/06_6_draw_lollipop.py
@@ -16,11 +16,13 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
+    print(args.gene)
 
     input_data = pandas.read_csv(args.input, sep="\t", index_col=0)
     print(input_data)
 
     for gene in tqdm.tqdm(args.gene):
+        print(gene)
         gene_data = input_data[(input_data["Hugo_Symbol"] == gene) & ~(input_data["HGVSp_Short"].isna())]
         print(gene_data)
 
@@ -32,7 +34,7 @@ def main():
             proteins += f"{row['HGVSp_Short'][2:]} "
         proteins = proteins.strip()
 
-        os.system(f"/BiO/Share/Tools/lollipops -legend -labels -o {args.output}/{args.gene}.png -dpi=600 -show-motifs {gene} {proteins}")
+        os.system(f"/BiO/Share/Tools/lollipops -legend -labels -o {args.output}/{gene}.png -dpi=600 -show-motifs {gene} {proteins}")
 
 if __name__ == "__main__":
     main()
