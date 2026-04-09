6c93f39b0a76927c1cd738deda780aa84546905a
Jaewoong Lee <230@fumire.moe>
2026-04-09 10:00:39 +0900

    Add only normal samples
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- 04_Germline_short_variant_discovery/04_1_HaplotypeCaller.bash
+++ 04_Germline_short_variant_discovery/04_1_HaplotypeCaller.bash
@@ -3,4 +3,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 python3 -B 04_1_HaplotypeCaller.py "$(realpath ../02_Data_pre-processing_for_variant_discovery/C001_TN.Sort.MarkDuplicates.BQSR.bam)" "$(realpath ./C001_TN.maf)"
-python3 -B 04_1_HaplotypeCaller.py "$(realpath ../02_Data_pre-processing_for_variant_discovery/C001_TT.Sort.MarkDuplicates.BQSR.bam)" "$(realpath ./C001_TT.maf)"
+python3 -B 04_1_HaplotypeCaller.py "$(realpath ../02_Data_pre-processing_for_variant_discovery/C002_TN.Sort.MarkDuplicates.BQSR.bam)" "$(realpath ./C002_TN.maf)"
+python3 -B 04_1_HaplotypeCaller.py "$(realpath ../02_Data_pre-processing_for_variant_discovery/C003_TN.Sort.MarkDuplicates.BQSR.bam)" "$(realpath ./C003_TN.maf)"
+python3 -B 04_1_HaplotypeCaller.py "$(realpath ../02_Data_pre-processing_for_variant_discovery/K001_BU.Sort.MarkDuplicates.BQSR.bam)" "$(realpath ./K001_BU.maf)"
+python3 -B 04_1_HaplotypeCaller.py "$(realpath ../02_Data_pre-processing_for_variant_discovery/K002_BU.Sort.MarkDuplicates.BQSR.bam)" "$(realpath ./K002_BU.maf)"
+python3 -B 04_1_HaplotypeCaller.py "$(realpath ../02_Data_pre-processing_for_variant_discovery/K003_BU.Sort.MarkDuplicates.BQSR.bam)" "$(realpath ./K003_BU.maf)"
